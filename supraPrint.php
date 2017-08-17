c101bca4a50bcae35384e4c401784dcad4e8d011
Ekapop <eploentham@gmail.com>
2017-08-17 21:54:58 +0700

    print
    bangna5

--- supraPrint.php
+++ supraPrint.php
@@ -18,6 +18,7 @@ $doctorName="";
 $staff="";
 $reason="";
 $now = getdate();
+$curDate = date("d-m-Y");
 if(isset($_GET["supra_id"])){
     $supraId = $_GET["supra_id"];
 }else{
@@ -51,11 +52,11 @@ if ($rComp=mysqli_query($conn,$sql)){
         //$hosp = $sql;<i class="glyphicon glyphicon-glyphicon-unchecked"></i>
     }
 }
-$paid='<table width="100%"><tr></tr><td><span class="glyphicon glyphicon-check" >&nbsp;ประกันสังคม &nbsp;'.$br1.'</span></td><td><span class="glyphicon glyphicon-unchecked" >&nbsp;กองทุน – บริษัท</span></td><td><span class="glyphicon glyphicon-unchecked" >&nbsp;ทั่วไป</span></td></tr><tr><td><span class="glyphicon glyphicon-unchecked" aria-hidden="true">&nbsp;พรบ..............................</span></td><td><span class="glyphicon glyphicon-unchecked" aria-hidden="true">&nbsp;ประกันสุขภาพ.........................</span></td><td><span class="glyphicon glyphicon-unchecked" aria-hidden="true">&nbsp;อื่น ๆ.........................</span></td></tr></table>';
+$paid='<table width="100%"><tr></tr><td class="tdliner"><img src="img/checked.png" alt="me" >&nbsp;ประกันสังคม &nbsp;'.$br1.'</td><td class="tdliner"><img src="img/unchecked.png" alt="me" >&nbsp;กองทุน – บริษัท</td><td><img src="img/unchecked.png" alt="me" >&nbsp;ทั่วไป</td></tr><tr><td class="tdliner"><img src="img/unchecked.png" alt="me" >&nbsp;พรบ..............................</td><td class="tdliner"><img src="img/unchecked.png" alt="me" >&nbsp;ประกันสุขภาพ.........................</td><td><img src="img/unchecked.png" alt="me" >&nbsp;อื่น ๆ.........................</td></tr></table>';
 if(trim($sex)==="M"){
-    $sex='เพศ &nbsp;<span class="glyphicon glyphicon-check">&nbsp;ชาย</span> &nbsp;<span class="glyphicon glyphicon-unchecked">&nbsp;หญิง</span> ';
+    $sex='เพศ &nbsp;<img src="img/checked.png" alt="me" >&nbsp;ชาย &nbsp;<img src="img/unchecked.png" alt="me" >&nbsp;หญิง ';
 }else{
-    $sex='เพศ &nbsp;<span class="glyphicon glyphicon-uncheck">&nbsp;ชาย</span> &nbsp;<span class="glyphicon glyphicon-checked">&nbsp;หญิง</span> ';
+    $sex='เพศ &nbsp;<img src="img/unchecked.png" alt="me" >&nbsp;ชาย &nbsp;<img src="img/checked.png" alt="me" >&nbsp;หญิง ';
 }
 if(strlen($diseased1)>0){
     $diseased1 = "โรค     1. ".$diseased1;
@@ -80,32 +81,32 @@ if(strlen($staff)>0){
 if(strlen($reason)>0){
     if($reason==="1"){
         $reason = '<table width="100%"><tr><td colspan="3">เหตุผลในการส่งตัวเพื่อ</td></tr>'
-            . '<tr><td><span class="glyphicon glyphicon-check" >&nbsp;การวินิจฉัย &nbsp;</span></td><td><span class="glyphicon glyphicon-unchecked" >&nbsp;รักษาจนเสร็จ</span></td><td><span class="glyphicon glyphicon-unchecked" >&nbsp;ขอทราบผล</span></td></tr>'
-            . '<tr><td><span class="glyphicon glyphicon-unchecked" >&nbsp;รักษาเบื้องต้น</span></td><td><span class="glyphicon glyphicon-unchecked" >&nbsp;อื่นๆ</span></td><td>&nbsp;</td></tr></table>';
+            . '<tr><td><img src="img/checked.jpg" alt="me" >&nbsp;การวินิจฉัย &nbsp;</td><td><img src="img/unchecked.png" alt="me" >&nbsp;รักษาจนเสร็จ</td><td><img src="img/unchecked.png" alt="me" >&nbsp;ขอทราบผล</td></tr>'
+            . '<tr><td><img src="img/unchecked.jpg" alt="me" >&nbsp;รักษาเบื้องต้น</td><td><img src="img/unchecked.png" alt="me" >&nbsp;อื่นๆ</td><td>&nbsp;</td></tr></table>';
     }else if($reason==="2"){
         $reason = '<table width="100%"><tr><td colspan="3">เหตุผลในการส่งตัวเพื่อ</td></tr>'
-            . '<tr><td><span class="glyphicon glyphicon-unchecked" >&nbsp;การวินิจฉัย &nbsp;</span></td><td><span class="glyphicon glyphicon-check" >&nbsp;รักษาจนเสร็จ</span></td><td><span class="glyphicon glyphicon-unchecked" >&nbsp;ขอทราบผล</span></td></tr>'
-            . '<tr><td><span class="glyphicon glyphicon-unchecked" >&nbsp;รักษาเบื้องต้น</span></td><td><span class="glyphicon glyphicon-unchecked" >&nbsp;อื่นๆ</span></td><td>&nbsp;</td></tr></table>';
+            . '<tr><td><img src="img/unchecked.jpg" alt="me" >&nbsp;การวินิจฉัย &nbsp;</td><td><img src="img/unchecked.png" alt="me" >&nbsp;รักษาจนเสร็จ</td><td><img src="img/unchecked.png" alt="me" >&nbsp;ขอทราบผล</td></tr>'
+            . '<tr><td><img src="img/unchecked.jpg" alt="me" >&nbsp;รักษาเบื้องต้น</td><td><img src="img/unchecked.png" alt="me" >&nbsp;อื่นๆ</td><td>&nbsp;</td></tr></table>';
     }else if($reason==="3"){
         $reason = '<table width="100%"><tr><td colspan="3">เหตุผลในการส่งตัวเพื่อ</td></tr>'
-            . '<tr><td><span class="glyphicon glyphicon-unchecked" >&nbsp;การวินิจฉัย &nbsp;</span></td><td><span class="glyphicon glyphicon-unchecked" >&nbsp;รักษาจนเสร็จ</span></td><td><span class="glyphicon glyphicon-check" >&nbsp;ขอทราบผล</span></td></tr>'
-            . '<tr><td><span class="glyphicon glyphicon-unchecked" >&nbsp;รักษาเบื้องต้น</span></td><td><span class="glyphicon glyphicon-unchecked" >&nbsp;อื่นๆ</span></td><td>&nbsp;</td></tr></table>';
+            . '<tr><td><img src="img/unchecked.jpg" alt="me" >&nbsp;การวินิจฉัย &nbsp;</td><td><img src="img/unchecked.png" alt="me" >&nbsp;รักษาจนเสร็จ</td><td><img src="img/checked.png" alt="me" >&nbsp;ขอทราบผล</td></tr>'
+            . '<tr><td><img src="img/unchecked.jpg" alt="me" >&nbsp;รักษาเบื้องต้น</td><td><img src="img/unchecked.png" alt="me" >&nbsp;อื่นๆ</td><td>&nbsp;</td></tr></table>';
     }else if($reason==="4"){
         $reason = '<table width="100%"><tr><td colspan="3">เหตุผลในการส่งตัวเพื่อ</td></tr>'
-            . '<tr><td><span class="glyphicon glyphicon-unchecked" >&nbsp;การวินิจฉัย &nbsp;</span></td><td><span class="glyphicon glyphicon-unchecked" >&nbsp;รักษาจนเสร็จ</span></td><td><span class="glyphicon glyphicon-unchecked" >&nbsp;ขอทราบผล</span></td></tr>'
-            . '<tr><td><span class="glyphicon glyphicon-check" >&nbsp;รักษาเบื้องต้น</span></td><td><span class="glyphicon glyphicon-unchecked" >&nbsp;อื่นๆ</span></td><td>&nbsp;</td></tr></table>';
+            . '<tr><td><img src="img/unchecked.jpg" alt="me" >&nbsp;การวินิจฉัย &nbsp;</td><td><img src="img/unchecked.png" alt="me" >&nbsp;รักษาจนเสร็จ</td><td><img src="img/unchecked.png" alt="me" >&nbsp;ขอทราบผล</td></tr>'
+            . '<tr><td><img src="img/checked.jpg" alt="me" >&nbsp;รักษาเบื้องต้น</td><td><img src="img/unchecked.png" alt="me" >&nbsp;อื่นๆ</td><td>&nbsp;</td></tr></table>';
     }else if($reason==="5"){
         $reason = '<table width="100%"><tr><td colspan="3">เหตุผลในการส่งตัวเพื่อ</td></tr>'
-            . '<tr><td><span class="glyphicon glyphicon-unchecked" >&nbsp;การวินิจฉัย &nbsp;</span></td><td><span class="glyphicon glyphicon-unchecked" >&nbsp;รักษาจนเสร็จ</span></td><td><span class="glyphicon glyphicon-unchecked" >&nbsp;ขอทราบผล</span></td></tr>'
-            . '<tr><td><span class="glyphicon glyphicon-unchecked" >&nbsp;รักษาเบื้องต้น</span></td><td><span class="glyphicon glyphicon-check" >&nbsp;อื่นๆ</span></td><td>&nbsp;</td></tr></table>';
+            . '<tr><td><img src="img/unchecked.jpg" alt="me" >&nbsp;การวินิจฉัย &nbsp;</td><td><img src="img/unchecked.png" alt="me" >&nbsp;รักษาจนเสร็จ</td><td><img src="img/unchecked.png" alt="me" >&nbsp;ขอทราบผล</td></tr>'
+            . '<tr><td><img src="img/unchecked.jpg" alt="me" >&nbsp;รักษาเบื้องต้น</td><td><img src="img/checked.png" alt="me" >&nbsp;อื่นๆ</td><td>&nbsp;</td></tr></table>';
     }else{
         $reason="error";
     }
     
 }else{
     $reason = '<table width="100%"><tr><td colspan="3">เหตุผลในการส่งตัวเพื่อ</td></tr>'
-        . '<tr><td><span class="glyphicon glyphicon-uncheck" >&nbsp;การวินิจฉัย &nbsp;</span></td><td><span class="glyphicon glyphicon-unchecked" >&nbsp;รักษาจนเสร็จ</span></td><td><span class="glyphicon glyphicon-unchecked" >&nbsp;ขอทราบผล</span></td></tr>'
-        . '<tr><td><span class="glyphicon glyphicon-unchecked" >&nbsp;รักษาเบื้องต้น</span></td><td><span class="glyphicon glyphicon-unchecked" >&nbsp;อื่นๆ</span></td><td>&nbsp;</td></tr></table>';
+        . '<tr><td>&nbsp;การวินิจฉัย &nbsp;</td><td><img src="img/unchecked.jpg" alt="me" >&nbsp;รักษาจนเสร็จ</td><td>&nbsp;ขอทราบผล</td></tr>'
+        . '<tr><td><img src="img/unchecked.jpg" alt="me" >&nbsp;รักษาเบื้องต้น</td><td><img src="img/unchecked.jpg" alt="me" >&nbsp;อื่นๆ</td><td>&nbsp;</td></tr></table>';
 }
 $rComp->free();
 mysqli_close($conn);
@@ -152,6 +153,18 @@ mysqli_close($conn);
 	border-bottom: solid 1px #f0f0f0;
         margin: 0;
     }
+    .tdlineb{
+        border-bottom: solid 1px #000;
+    }
+    .tdlinet{
+        border-top:solid 1px #000;
+    }
+    .tdlinel{
+        border-left:solid 1px #000;
+    }
+    .tdliner{
+        border-right:solid 1px #000;
+    }
 </style>
   
 
@@ -167,106 +180,104 @@ mysqli_close($conn);
                 </tr>
             </table>
         </div>
-    </div>
+    </div><br>
     <div class="row">
         <div class="col col-sm-12">
             <table class="table table-striped table-bordered table-hover responsive"  width="100%">
                 <tbody>
-                    <tr><td >ใบรับรองการส่งตัวผู้ป่วยไปตรวจนอกสถานที่</td></tr>
+                    <tr><td align="center" class="padding-5">ใบรับรองการส่งตัวผู้ป่วยไปตรวจนอกสถานที่</td></tr>
                 </tbody>
             </table>
         </div>
-    </div>
+    </div><br>
     <div class="row">
         <div class="col col-sm-12">
             <table class="table table-striped table-bordered table-hover responsive"  width="100%">
                 <tbody>
-                    <tr><td><?php echo 'เลขที่ '.$doc;?></td><td><?php echo 'วันที่ '.$now['mday'].' '.$now['mon'].' ',$now['year'];?></td></tr>
+                    <tr><td align="left"><?php echo 'เลขที่ '.$doc;?></td><td align="right"><?php echo 'วันที่ '.$curDate;?></td></tr>
                 </tbody>
             </table>
         </div>
-    </div>
+    </div><br>
     <div class="row">
         <div class="col col-sm-12">
-            รพ. / สถานที่รับ
+            <h4>รพ. / สถานที่รับ</h4>
         </div>
     </div>
     <div class="row">
         <div class="col col-sm-12">
-            <table class="header-print topbar-v1"  width="100%" id="aa">
+            <table class="header-print topbar-v1"  width="100%" id="aa" >
                 <tbody>
-                    <tr><td><?php echo $br;?></td><td><?php echo $brCode;?></td><td></td></tr>
-                    <tr><td colspan="3"><?php echo $hosp;?></td></tr>
-                    <tr><td colspan="3"><?php echo $paid;?></td></tr>
+                    <tr><td class="tdlinel tdlineb tdlinet"><?php echo $br;?></td><td colspan="2" class="tdlineb tdlinet tdlinel tdliner"><?php echo $brCode;?></td></tr>
+                    <tr><td colspan="3" class="tdlinel tdlineb tdliner"><?php echo $hosp;?></td></tr>
+                    <tr><td colspan="3" class="tdlinel tdlineb tdliner"><?php echo $paid;?></td></tr>
                 </tbody>
             </table>
         </div>
-    </div>
+    </div><br>
     <div class="row">
         <div class="col col-sm-12">
-            สำหรับผู้ส่ง
+            <h4>สำหรับผู้ส่ง</h4>
         </div>
     </div>
     <div class="row">
         <div class="col col-sm-12">
-            <table class="table table-striped table-bordered table-hover responsive"  width="100%">
+            <table class="table table-striped table-bordered table-hover responsive"  width="100%" border="1">
                 <tbody>
                     <tr><td class="name"><?php echo $name;?></td><td class="hn"><?php echo $hn;?></td><td class="hn">AN - </td></tr>
                     <tr><td class="name"><?php echo $id;?></td><td class="hn"><?php echo $sex;?></td><td class="hn"><?php echo $age;?></td></tr>
                 </tbody>
             </table>
         </div>
-    </div>
-    <div class="row">
-        <div class="col col-sm-6">
-            <?php echo $diseased1;?>
-        </div>
-        <div class="col col-sm-6">
-            <?php echo $diseased2;?>
-        </div>
-    </div>
+    </div><br>
+    <table>
+        <tr><td align="left"><?php echo $diseased1;?></td><td align="left"><?php echo $diseased2;?></td></tr>
+    </table>
+
     <div class="row">
         <div class="col col-sm-12">
             <?php echo $reason;?>
         </div>
-    </div>
-    <div class="row">
-        <div class="col col-sm-12">ข้อกำหนดและความต้องการของผู้ส่ง</div>
-    </div>
-    <div class="row">
-        <div class="col col-sm-1">&nbsp;</div>
-        <div class="col col-sm-11">ต้องการส่งผู้ป่วยโดยคาดว่าจะถึงผู้รับ	     วันที่ 25-28 สิงหาคม 2560 เวลา09.30-10.00 น.           โดยรถพยาบาล   </div>
-    </div>
-    <div class="row">
-        <div class="col col-sm-1">&nbsp;</div>
-        <div class="col col-sm-11">ผู้รับโปรดแจ้งผู้ส่งเมื่อวงเงินการรักษาเกิน...................................................	บาท</div>
-    </div>
-    <div class="row">
-        <div class="col col-sm-1">&nbsp;</div>
-        <div class="col col-sm-11">กรณีผู้ป่วยในผู้รับโปรดแจ้งผู้ส่งเป็นประจำทุกวัน</div>
-    </div>
-    <div class="row">
-        <div class="col col-sm-1">&nbsp;</div>
-        <div class="col col-sm-7">หมายเหตุ : กรณีผู้ป่วยใช้สิทธิประกันสังคมทางโรงพยาบาล</div>
-        <div class="col col-sm-4">ลงชื่อ................................................................</div>
-    </div>
-    <div class="row">
-        <div class="col col-sm-1">&nbsp;</div>
-        <div class="col col-sm-7">จะรับผิดชอบค่ารักษาตามสิทธิ์ยกเว้นยานอกบัญชียาหลัก</div>
-        <div class="col col-sm-4">( นายแพทย์อรรถสิทธ์    ทองปลาเค้า )</div>
-    </div>
-    <div class="row">
-        <div class="col col-sm-1">&nbsp;</div>
-        <div class="col col-sm-7">&nbsp;</div>
-        <div class="col col-sm-4">ผู้อำนวยการโรงพยาบาลบางนา 5</div>
-    </div>
+    </div><br>
+    <table width="100%">
+        <tr>
+            <td colspan="2" align="left">ข้อกำหนดและความต้องการของผู้ส่ง</td>
+        </tr>
+        <tr>
+            <td colspan="2" align="left">ต้องการส่งผู้ป่วยโดยคาดว่าจะถึงผู้รับ	     วันที่ 25-28 สิงหาคม 2560 เวลา09.30-10.00 น.           โดยรถพยาบาล</td>
+        </tr>
+        <tr>
+            <td colspan="2" align="left">ผู้รับโปรดแจ้งผู้ส่งเมื่อวงเงินการรักษาเกิน...................................................	บาท</td>
+        </tr>
+        <tr>
+            <td colspan="2" align=""left>กรณีผู้ป่วยในผู้รับโปรดแจ้งผู้ส่งเป็นประจำทุกวัน</td>
+        </tr>
+        <tr>
+            <td align="left"><table>
+                    <tr><td><b>หมายเหตุ : กรณีผู้ป่วยใช้สิทธิประกันสังคมทางโรงพยาบาล</b></td></tr>
+                    <tr><td><b>จะรับผิดชอบค่ารักษาตามสิทธิ์ยกเว้นยานอกบัญชียาหลัก</b></td></tr>
+                    <tr><td>&nbsp;</td></tr>
+                    <tr><td>&nbsp;</td></tr>
+                </table>
+            </td>
+            <td align="right">
+                <table>
+                    <tr><td>&nbsp;</td></tr>
+                    <tr><td>ลงชื่อ................................................................</td></tr>
+                    <tr><td align="center">( นายแพทย์อรรถสิทธ์    ทองปลาเค้า )</td></tr>
+                    <tr><td align="center">ผู้อำนวยการโรงพยาบาลบางนา 5</td></tr>
+                </table>
+            </td>
+        </tr>
+    </table>
+    <br>
     <div class="row">
         <div class="col col-sm-12">สำหรับผู้ประสานงานติดต่อ</div>
     </div>
-    <table>
+    <table width="100%">
         <tr>
-            <td>
-                <table>
+            <td align="top" width="50%">
+                <table border="1">
                     <tr><td>ผู้ส่ง (ให้ลงข้อมูลผู้รับผิดชอบการติดต่อ)</td></tr>
                     <tr><td>ชื่อ – สกุล  นายวุฒินันท์ ธัมมาภิรักษ์กุล</td></tr>
                     <tr><td>ตำแหน่ง  เจ้าหน้าที่ประกันสังคม    แผนก  ประกันสังคม</td></tr>
@@ -274,22 +285,24 @@ mysqli_close($conn);
                     <tr><td>ได้ติดต่อเบื้องต้นแล้วกับ</td></tr>
                     <tr><td>ชื่อ – สกุล.........................................................................................</td></tr>
                     <tr><td>
-                        <table><tr><td><span class="glyphicon glyphicon-unchecked" >จะมีใบส่งตัวผู้ป่วย</span></td><td><span class="glyphicon glyphicon-unchecked" >จะมีจดหมายส่งตัว</span></td><td></td></tr>
+                        <table><tr><td><img src="img/unchecked.png" alt="me" >จะมีใบส่งตัวผู้ป่วย</td><td><img src="img/unchecked.png" alt="me" >จะมีจดหมายส่งตัว</td><td></td></tr>
                         <!--<tr><td><span class="glyphicon glyphicon-unchecked" >FAX</span></td><td><span class="glyphicon glyphicon-unchecked" >โทรศัพท์ </span></td><td><span class="glyphicon glyphicon-unchecked" >อื่นๆ........................ </span></td></tr></table></td></tr>-->
-                            <tr><td><span class="glyphicon glyphicon-unchecked" >FAX</span></td><td><span class="glyphicon glyphicon-unchecked" >โทรศัพท์ </span></td><td><span class="glyphicon glyphicon-unchecked" >อื่นๆ........................ </span></td></tr></table></td></tr>
+                            <tr><td><img src="img/unchecked.png" alt="me" >FAX<img src="img/unchecked.png" alt="me" >โทรศัพท์ </td><td><img src="img/unchecked.png" alt="me" >อื่นๆ........................ </td></tr></table></td></tr>
                         
                     
                 </table>
             </td>
-            <td>
-                <table>
+            <td align="top" width="50%">
+                <table border="1" align="right">
                     <tr><td>ผู้รับ (ให้ลงข้อมูลผู้รับผิดชอบการติดต่อ)</td></tr>
                     <tr><td>ชื่อ – สกุล......................................................................................</td></tr>
                     <tr><td>ตำแหน่ง..............................................แผนก.................................</td></tr>
                     <tr><td>วันที่..................................................เวลา........................ น.</td></tr>
                     <tr><td>ให้ผู้ส่งดำเนินการได้ทันที</td></tr>
                     <tr><td>ผู้รับจะติดต่อกลับไปภายในวันที่....................................</td></tr>
-                    <tr><td><span class="glyphicon glyphicon-unchecked" >โทรศัพท์</span><span class="glyphicon glyphicon-unchecked" >อื่นๆ..................................</span></td></tr>
+                    <tr><td><img src="img/unchecked.png" alt="me" >โทรศัพท์<img src="img/unchecked.png" alt="me" >อื่นๆ..................................</td></tr>
+                    <tr border="0"><td>&nbsp;</td></tr>
+                    <tr border="0"><td>&nbsp;</td></tr>
                 </table>
             </td>
         </tr>
